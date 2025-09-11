6d41aee61bc7d460e0966d0965125406a5a5fa80
Ivan Klochkov <vany-nov13@yandex.ru>
2025-09-11 14:24:18 -0700

    feat: add solution for the second task

--- mod4/lab4.2/index.js
+++ mod4/lab4.2/index.js
@@ -27,3 +27,26 @@ function displayList() {
   }
   return shoppingList.forEach(item => console.log(item));
 }
+
+// Task 2: Filter and Search an Array
+
+function addNewItem(item) {
+  if (!shoppingList.includes(item)) shoppingList.push(item);
+  return shoppingList;
+}
+
+// shoppingList = ["banana", "apple", "orange"];
+console.log(addNewItem('banana')); // ["banana", "apple", "orange"]
+console.log(addNewItem('grape')); // ["banana", apple, "orange", "grape"]
+
+function filterItems(searchTerm) {
+  const lowerCaseTerm = searchTerm.toLowerCase(); // just for me
+  return shoppingList.filter(item =>
+    item.toLowerCase().includes(lowerCaseTerm)
+  );
+}
+
+// shoppingList = ['banana', 'apple', 'orange'];
+
+console.log(filterItems('banana')); // ["banana", "apple", "orange"]
+console.log(filterItems('grape')); // ["banana", "apple", "orange", "grape"]
