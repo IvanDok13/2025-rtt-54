7f207e2075e49ae37535718dfbf8f233cef984d2
Ivan Klochkov <vany-nov13@yandex.ru>
2025-09-22 16:22:19 -0700

    fix: remove styles and unnecessary clases

--- mod4/sba4/app.js
+++ mod4/sba4/app.js
@@ -8,6 +8,8 @@ const emptyInfo = document.getElementById('emptyInfo');
 
 const taskListUl = document.createElement('ul');
 taskListUl.id = 'task-list';
+taskListUl.className =
+  'mt-[18px] grid grid-cols-[repeat(auto-fill,minmax(280px,1fr))] gap-[14px]';
 taskListSection.appendChild(taskListUl);
 
 // Arrays of Tasks
@@ -50,4 +52,20 @@ function toast(message) {
   setTimeout(() => {
     toastEl.classList.remove('opacity-100');
   }, 3000);
+}
+
+function displayTasks() {
+  const task = document.createElement('li');
+  task.className =
+    'border-[#2a2f3a] rounded-[16px] p-[14px] shadow-[0_10px_30px_rgba(0,0,0,0.35)] flex flex-col gap-3 relative transition-transform duration-150 ease-linear hover:-translate-y-0.5';
+
+  task.innerHTML = `
+    <h3 class="text-lg font-semibold mb-2">${tasks[tasks.length - 1].taskName}</h3>
+    <p class="mb-1"><strong>Category:</strong> ${tasks[tasks.length - 1].category}</p>
+    <p class="mb-1"><strong>Deadline:</strong> ${tasks[tasks.length - 1].deadline}</p>
+    <p><strong>Status:</strong> ${tasks[tasks.length - 1].status}</p>
+    <button class="mt-2 px-3 py-1 bg-red-400 text-white rounded delete-btn">Delete</button>
+  `;
+
+  taskListUl.appendChild(task);
 }
