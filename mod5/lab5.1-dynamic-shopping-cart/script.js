6060b5110dab0078821349f33ba7c6cd06201dc7
Ivan Klochkov <vany-nov13@yandex.ru>
2025-09-24 15:02:36 -0700

    feat: add renderList func

--- mod5/lab5.1-dynamic-shopping-cart/script.js
+++ mod5/lab5.1-dynamic-shopping-cart/script.js
@@ -14,8 +14,29 @@ function updateTotalPrice(amount) {
 
 // Function to remove an item
 function removeItem(event) {
-  const item = event.target.closest('li');
+  const item = event.target.closest('div');
   const price = parseFloat(item.dataset.price);
   updateTotalPrice(-price);
   item.remove();
 }
+
+function renderList() {
+  if (productNameInput.value.trim() !== '') {
+    const li = document.createElement('div');
+    const btn = document.createElement('button');
+    const priceSpan = document.createElement('span');
+
+    const ammount = +productPriceInput.value;
+    li.dataset.price = ammount;
+    priceSpan.textContent = ammount;
+    btn.textContent = 'Remove';
+    li.textContent = productNameInput.value;
+
+    cart.appendChild(li);
+    li.appendChild(priceSpan);
+    li.appendChild(btn);
+    updateTotalPrice(ammount);
+  }
+}
+
+addProductButton.addEventListener('click', renderList);
