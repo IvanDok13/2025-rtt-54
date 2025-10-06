089d4c47fde8013584cdb35630b6ab800dde4258
Ivan Klochkov <vany-nov13@yandex.ru>
2025-10-06 15:57:53 -0700

    fix: update elements

--- mod5/sba5/script.js
+++ mod5/sba5/script.js
@@ -5,29 +5,26 @@ const postsState = [];
 // DOM Element Selection
 
 const postForm = document.getElementById('post-form');
-const titleInput = document.getElementById('title-input');
+const titleInput = document.getElementById('title');
 const titleError = document.getElementById('title-error');
-const contentInput = document.getElementById('content-input');
+const contentInput = document.getElementById('content');
 const contentError = document.getElementById('content-error');
 const postsContainer = document.getElementById('posts-list');
 const submitBtn = document.getElementById('submit-btn');
 const charCount = document.getElementById('char-count');
+const emptyInfo = document.getElementById('empty-info');
 
 // Load Posts from localStorage
 
 document.addEventListener('DOMContentLoaded', () => {
   const savedPost = localStorage.getItem('savedPost');
   if (savedPost) {
-    const { title, content } = JSON.parse(savedPost);
-    titleInput.value = title;
-    contentInput.value = content;
+    const parsedPosts = JSON.parse(savedPosts);
+    postsState.length = 0;
+    postsState.push(...parsedPosts);
+    // renderPosts();
     // updateCharCount();
   }
 });
 
 // Render Posts Function
-
-function createPostElement(post, index) {
-  titleInput.value.trim();
-  contentInput.value.trim();
-}
