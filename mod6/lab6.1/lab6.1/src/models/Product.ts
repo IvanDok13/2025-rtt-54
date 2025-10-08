28442e6af8550179b762ac9ac078af4edd4b0a9f
Ivan Klochkov <vany-nov13@yandex.ru>
2025-10-08 13:34:41 -0700

    feat: create base product class

---
+++ mod6/lab6.1/lab6.1/src/models/Product.ts
@@ -0,0 +1,18 @@
+class Product {
+  sku: string;
+  name: string;
+  price: number;
+  constructor(sku: string, name: string, price: number) {
+    this.sku = sku;
+    this.name = name;
+    this.price = price;
+  }
+
+  displayDetails() {
+    return `sku: ${this.sku}, name: ${this.name}, price: ${this.price}`;
+  }
+
+  getPriceWithTax() {
+    return this.price * 1.1;
+  }
+}
