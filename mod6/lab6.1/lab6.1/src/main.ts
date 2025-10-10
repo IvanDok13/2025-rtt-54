910d3057662201ce423270f78ce63469ecadd0a1
Ivan Klochkov <vany-nov13@yandex.ru>
2025-10-10 11:35:57 -0700

    feat: add displaying products and total price

--- mod6/lab6.1/lab6.1/src/main.ts
+++ mod6/lab6.1/lab6.1/src/main.ts
@@ -1,3 +1,12 @@
-// Inside src/main.ts, import the PhysicalProduct and DigitalProduct classes, and create instances of both.
-// Use a loop to display the details of each product, calculate prices with tax, and display the final prices.
-// Hint: Utilize polymorphism to your advantage here.
+import { DigitalProduct } from './models/DigitalProduct.js';
+import { PhysicalProduct } from './models/PhysicalProduct.js';
+
+const products = [
+  new PhysicalProduct('M-BLD-001', 'Laptop', 1000, 20),
+  new DigitalProduct('M-DIG-002', 'Camera', 500, 5),
+];
+
+products.forEach(product => {
+  console.log(`Product: ${product.displayDetails()}`);
+  console.log(`Price with tax: ${product.getPriceWithTax()}`);
+});
