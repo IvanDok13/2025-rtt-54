6368250c96ebc7d1b8a325b45a04e3460ca8de62
Ivan Klochkov <vany-nov13@yandex.ru>
2025-10-27 01:47:46 -0700

    feat: add Products class

---
+++ mod6/sba6/src/models/Product.ts
@@ -0,0 +1,48 @@
+export default class Product {
+  title: string;
+  price: number;
+  id: number;
+  discountPercentage: number;
+  rating?: number;
+  stock?: number;
+  tags?: string[];
+
+  constructor(
+    title: string,
+    price: number,
+    id: number,
+    discountPercentage: number,
+    rating?: number,
+    stock?: number,
+    tags?: string[]
+  ) {
+    this.title = title;
+    this.price = price;
+    this.id = id;
+    this.discountPercentage = discountPercentage;
+    this.rating = rating;
+    this.stock = stock;
+    this.tags = tags;
+  }
+
+  displayProductDetails(this: Product): void {
+    console.log(`Product ID: ${this.id}`);
+    console.log(`Title: ${this.title}`);
+    console.log(`Price: $${this.price}`);
+    console.log(`Discount Percentage: ${this.discountPercentage}%`);
+    if (this.rating !== undefined) {
+      console.log(`Rating: ${this.rating}`);
+    }
+    if (this.stock !== undefined) {
+      console.log(`Stock: ${this.stock}`);
+    }
+    if (this.tags !== undefined) {
+      console.log(`Tags: ${this.tags.join(', ')}`);
+    }
+  }
+
+  getPriceWithDiscount(this: Product): number {
+    const discountAmount = (this.price * this.discountPercentage) / 100;
+    return this.price - discountAmount;
+  }
+}
