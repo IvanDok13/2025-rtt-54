4e161cfd14e5824bed2bb7728a2b7efa3f45fa47
Ivan Klochkov <vany-nov13@yandex.ru>
2025-10-28 00:18:12 -0700

    fix: remove comment, update func arguments

--- mod6/sba6/src/utils/discountCalculator.ts
+++ mod6/sba6/src/utils/discountCalculator.ts
@@ -1,13 +1,10 @@
-// Discount Calculator Module (discountCalculator.ts):
-// Create a calculateDiscount() function to handle discount calculations for products.
-// This function should return the dollar amount that a product is discounted by. For example, if a product costs $100 and has a 10% discount, the function should return $10.
-
-import type Product from '../models/Product';
-
-export function calculateDiscount(this: Product): number {
-  if (this.discountPercentage < 0 || this.discountPercentage > 100) {
+export function calculateDiscount(
+  price: number,
+  discountPercentage: number
+): number {
+  if (discountPercentage < 0 || discountPercentage > 100) {
     throw new Error('Discount percentage must be between 0 and 100.');
   }
-  const discountAmount = (this.price * this.discountPercentage) / 100;
+  const discountAmount = (price * discountPercentage) / 100;
   return discountAmount;
 }
