64b8579e86c2fa5b3c236b26389baf6d3ffadc77
Ivan Klochkov <vany-nov13@yandex.ru>
2025-10-30 11:02:54 -0700

    feat: update function result

--- mod6/sba6/src/utils/taxCalculator.ts
+++ mod6/sba6/src/utils/taxCalculator.ts
@@ -1,15 +1,15 @@
 import { GROCERIES_TAX_RATE, STANDART_TAX_RATE } from './const';
 import { ValidationError } from './errorHandler';
 
-export function calculateTax(price: number, categoty: string): string {
+export function calculateTax(price: number, categoty: string): number {
   if (price < 0) {
     throw new ValidationError('Price must be a positive number');
   }
 
   let taxRate = STANDART_TAX_RATE;
-  if (categoty.toLowerCase() === 'groceries') {
+  if (categoty === 'groceries') {
     taxRate = GROCERIES_TAX_RATE;
   }
 
-  return `Total prise with tax: $${(price * taxRate + price).toFixed(2)}`;
+  return +(price * taxRate + price).toFixed(2);
 }
