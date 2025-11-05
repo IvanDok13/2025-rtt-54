081f53e362e2ed3a214db16dc8a6e0c7cccb1413
Ivan Klochkov <vany-nov13@yandex.ru>
2025-11-04 23:35:53 -0800

    feat: update app with new components

--- mod9/lab9.2/src/App.tsx
+++ mod9/lab9.2/src/App.tsx
@@ -1,9 +1,22 @@
 import './App.css';
+import { StatsDisplay } from './components/StatsDisplay/StatsDisplay';
+import { TextInput } from './components/TextInput/TextInput';
 
 function App() {
   return (
     <>
       <h1>Lab 9.2</h1>
+      <TextInput
+        onTextChange={text => {
+          console.log('Text changed:', text);
+        }}
+        placeholder='Type your text here...'
+        initialValue='Hello, world!'
+      />
+      <StatsDisplay
+        stats={{ characterCount: 100, wordCount: 20, readingTime: 1 }}
+        showReadingTime={true}
+      />
     </>
   );
 }
