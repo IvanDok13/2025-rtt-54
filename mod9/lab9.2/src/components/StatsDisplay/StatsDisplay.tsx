d8cccc481198ff4d9c466880294b5b4ddc8fbf25
Ivan Klochkov <vany-nov13@yandex.ru>
2025-11-05 22:06:13 -0800

    feat: add styles and update naming

--- mod9/lab9.2/src/components/StatsDisplay/StatsDisplay.tsx
+++ mod9/lab9.2/src/components/StatsDisplay/StatsDisplay.tsx
@@ -9,16 +9,16 @@ export function StatsDisplay({
   return (
     <div className='p-4 border border-gray-300 rounded-lg bg-gray-50'>
       <h2 className='text-xl font-semibold mb-4'>Stats Display</h2>
-      <ul className='space-y-2'>
+      <ul className='space-y-2 flex flex-row gap-4'>
         <li>
-          <strong>Character Count:</strong> {characterCount}
+          <strong>Characters:</strong> {characterCount}
         </li>
         <li>
-          <strong>Word Count:</strong> {wordCount}
+          <strong>Words:</strong> {wordCount}
         </li>
         {showReadingTime && (
           <li>
-            <strong>Estimated Reading Time:</strong> {readingTime} minute
+            <strong>Reading Time:</strong> {readingTime} minute
             {readingTime !== 1 ? 's' : ''}
           </li>
         )}
