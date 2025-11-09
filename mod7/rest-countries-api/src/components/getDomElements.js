4d1092304d1c61ed11ffff8b125b43f58d607b1c
Ivan Klochkov <vany-nov13@yandex.ru>
2025-11-09 15:56:22 -0800

    feat: create loader component

---
+++ mod7/rest-countries-api/src/components/getDomElements.js
@@ -0,0 +1,3 @@
+export const countriesDiv = document.getElementById('countries');
+export const loadingDiv = document.getElementById('loading');
+export const errorDiv = document.getElementById('error');
