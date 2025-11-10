8168b1c54b752f2e23e424852e5f9ffb1c851eb2
Ivan Klochkov <vany-nov13@yandex.ru>
2025-11-09 16:10:25 -0800

    feat: add API const

--- mod7/rest-countries-api/src/const/index.js
+++ mod7/rest-countries-api/src/const/index.js
@@ -1,1 +1,5 @@
-export const URL = 'https://restcountries.com/v3.1/name/';
+const API_URL = 'https://restcountries.com/v3.1/all';
+const FIELDS =
+  'name,flags,population,region,capital,language,currency,subregion';
+
+export { API_URL, FIELDS };
