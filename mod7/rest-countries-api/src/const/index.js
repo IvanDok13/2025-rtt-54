ced47d2c2c14af34b5b9fcb0d598df230c0d1b84
Ivan Klochkov <vany-nov13@yandex.ru>
2025-11-09 21:33:27 -0800

    feat: update service;

--- mod7/rest-countries-api/src/const/index.js
+++ mod7/rest-countries-api/src/const/index.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://restcountries.com/v3.1/all';
+const API_URL = 'https://restcountries.com/v3.1/';
 const FIELDS =
   'name,flags,population,region,capital,language,currency,subregion';
 
--- mod7/rest-countries-api/src/service/service.js
+++ mod7/rest-countries-api/src/service/service.js
@@ -5,7 +5,7 @@ export async function fetchCountries() {
   showLoading();
 
   try {
-    const response = await fetch(`${API_URL}?fields=${FIELDS}`);
+    const response = await fetch(`${API_URL}all?fields=${FIELDS}`);
 
     if (!response.ok) {
       throw new Error(`Failed to fetch countries: ${response.status}`);
@@ -15,6 +15,24 @@ export async function fetchCountries() {
     return countries;
   } catch (error) {
     console.error('Error fetching countries:', error);
+    throw error;
+  }
+}
+
+export async function fetchCountryDetails(countryName) {
+  try {
+    const response = await fetch(
+      `https://restcountries.com/v3.1/name/${countryName}?fullText=true`
+    );
+
+    if (!response.ok) {
+      throw new Error('Country not found');
+    }
+
+    const countries = await response.json();
+    return countries[0];
+  } catch (error) {
+    console.error('Error fetching country details:', error);
     throw error;
   }
 }
