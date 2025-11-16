48519c7bc4a5aa62459fb44ec8b7900e2a733b04
Ivan Klochkov <vany-nov13@yandex.ru>
2025-11-15 16:40:38 -0800

    feat: import into app

--- mod10/lab10.1/src/App.tsx
+++ mod10/lab10.1/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import { AdvancedCounter } from './components/AdvancedCounter';
 
 function App() {
   return (
     <>
       <h1>Lab 10.1</h1>
+      <AdvancedCounter />
     </>
   );
 }
