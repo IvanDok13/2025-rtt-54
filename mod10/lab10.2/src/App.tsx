af1e84d221ee721a1cf0d7c6873afe38a864787e
Ivan Klochkov <vany-nov13@yandex.ru>
2025-11-16 18:55:37 -0800

    feat: create useDebounce hook and implement it in Debounce search demo

--- mod10/lab10.2/src/App.tsx
+++ mod10/lab10.2/src/App.tsx
@@ -1,7 +1,17 @@
 import './App.css';
+import DebounceSearchDemo from './components/DebounceSearchDemo';
+import PaginationDemo from './components/PaginationDemo';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Custom Hooks</h1>
+      <DebounceSearchDemo />
+      <hr />
+
+      <PaginationDemo />
+    </>
+  );
 }
 
 export default App;
