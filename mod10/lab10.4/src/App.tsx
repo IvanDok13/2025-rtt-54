e9abc5d1acd15c3ed6c5407f54dde6a805c245d4
Ivan Klochkov <vany-nov13@yandex.ru>
2025-11-19 10:11:29 -0800

    feat: add routes

--- mod10/lab10.4/src/App.tsx
+++ mod10/lab10.4/src/App.tsx
@@ -1,9 +1,29 @@
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { NavBar } from './components/NavBar';
+import { data } from './lib/posts';
+import { AdminPage } from './pages/AdminPage';
+import { BlogIndexPage } from './pages/BlogIndexPage';
+import { BlogPostPage } from './pages/BlogPostPage';
+import { HomePage } from './pages/HomePage';
+import { LoginPage } from './pages/LoginPage';
 
 function App() {
+  const [blogs] = useState(data);
   return (
     <>
       <h1>Lab 10.4</h1>
+      <NavBar />
+
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/blog' element={<BlogPostPage blogs={blogs} />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/admin' element={<AdminPage />} />
+
+        <Route path='/blog/:slug' element={<BlogIndexPage blogs={blogs} />} />
+      </Routes>
     </>
   );
 }
