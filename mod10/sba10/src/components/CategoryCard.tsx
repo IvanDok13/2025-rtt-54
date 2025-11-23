5d9bfa1bd271b042b81eba96dda95ef7595a13e4
Ivan Klochkov <vany-nov13@yandex.ru>
2025-11-22 20:42:44 -0800

    feat: create category card component

---
+++ mod10/sba10/src/components/CategoryCard.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import type { CategoryCardProps } from '../types';
+
+export function CategoryCard({ category }: CategoryCardProps) {
+  return (
+    <Link
+      to={`/category/${category.strCategory}`}
+      className='block bg-white rounded-xl p-3 text-center no-underline text-gray-800 shadow-md hover:-translate-y-1 transition-transform duration-200'
+    >
+      <h3>{category.strCategory}</h3>
+
+      <img
+        src={category.strCategoryThumb}
+        alt={category.strCategory}
+        className='w-full rounded-lg mb-3'
+        width='200'
+      />
+    </Link>
+  );
+}
