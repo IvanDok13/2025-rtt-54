0e0d76300d9e6949d6c0004b4c5a627827b210b0
Ivan Klochkov <vany-nov13@yandex.ru>
2025-11-22 20:41:04 -0800

    feat: create spinner

---
+++ mod10/sba10/src/components/ui/Spinner.tsx
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div className='flex justify-center p-6 text-center'>
+      <div className='w-8 h-8 border-4 border-gray-300 border-t-green-600 rounded-full animate-spin' />
+    </div>
+  );
+}
