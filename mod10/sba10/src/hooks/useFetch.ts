31ea1af68b9a7a5b6a8dbd6f9b953079f98f5c30
Ivan Klochkov <vany-nov13@yandex.ru>
2025-11-22 20:50:13 -0800

    feat: add hook for fetching data

---
+++ mod10/sba10/src/hooks/useFetch.ts
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import type { Category } from '../types';
+
+export function useFetch(
+  fetcher: string | (() => Promise<Category>),
+  dependencies = []
+) {
+  const [data, setData] = useState<Category | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchFunction =
+    typeof fetcher === 'string'
+      ? () => fetch(fetcher).then(res => res.json())
+      : fetcher;
+
+  useEffect(() => {
+    let ignore = false;
+
+    async function load() {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const result = await fetchFunction();
+        if (!ignore) setData(result);
+      } catch (err: any) {
+        if (!ignore) setError(err.message);
+      } finally {
+        if (!ignore) setLoading(false);
+      }
+    }
+
+    load();
+
+    return () => {
+      ignore = true;
+    };
+  }, dependencies);
+
+  const refetch = () => {
+    setLoading(true);
+    setError(null);
+
+    fetchFunction()
+      .then(result => setData(result))
+      .catch(err => setError(err.message))
+      .finally(() => setLoading(false));
+  };
+
+  return { data, loading, error, refetch };
+}
