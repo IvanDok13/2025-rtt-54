500b67d0d0e8e32bd4c2b5658dc7104224c4dc20
Ivan Klochkov <vany-nov13@yandex.ru>
2025-11-23 03:04:11 -0800

    feat: add types for recipe

--- mod10/sba10/src/types/index.ts
+++ mod10/sba10/src/types/index.ts
@@ -9,6 +9,17 @@ export interface CategoryCardProps {
   category: Category;
 }
 
+export interface Recipe {
+  idMeal: string;
+  strMeal: string;
+  strMealThumb: string;
+  strInstructions: string;
+}
+
+export interface RecipeCardProps {
+  recipe: Recipe;
+}
+
 export interface EmptyStateProps {
   text?: string;
 }
