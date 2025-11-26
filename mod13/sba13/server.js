73dae2fd3930c82b19d30ed624ac0eb5bcb6bb2f
Ivan Klochkov <vany-nov13@yandex.ru>
2025-11-26 14:29:56 -0800

    feat: create server

---
+++ mod13/sba13/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const bookRoutes = require('./routes/bookRoutes');
+const { connectDB } = require('./db/connection');
+const morgan = require('morgan');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to our Tattoo Suplies Website!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
