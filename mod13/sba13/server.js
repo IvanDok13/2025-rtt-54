ddff4fb81978a5314a62b9465a8a18c963b22aec
Ivan Klochkov <vany-nov13@yandex.ru>
2025-11-26 15:06:47 -0800

    fix: remove unnecessary import

--- mod13/sba13/server.js
+++ mod13/sba13/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const bookRoutes = require('./routes/bookRoutes');
 const { connectDB } = require('./db/connection');
 const morgan = require('morgan');
 
