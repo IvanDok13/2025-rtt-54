852d3e097a3d46667838670619aa1a1dfcd1e2b2
Ivan Klochkov <vany-nov13@yandex.ru>
2025-11-26 14:11:17 -0800

    feat: add model and schema for it

---
+++ mod13/sba13/models/Product.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  inStock: {
+    type: Boolean,
+    default: true,
+  },
+  tags: [String],
+  createdAt: {
+    type: Date,
+  },
+});
+
+module.exports = mongoose.model('Product', productSchema);
